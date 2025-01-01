dbd00a6d7b9e77cf8dcd17e96ba2d76acba226d3
mseptiawan <mseptiawan017@gmail.com>
2025-01-01 10:59:38 +0700

    membuat halaman register
    1. tambah dependensi shared preference untuk simpan informasi login
    2. membuat register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,1 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? _email;
+  String? _password;
+
+  Future<void> _register() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      print('Password: $_password');
+
+      final prefs = await SharedPreferences.getInstance();
+
+      prefs.setString('email', _email!);
+      prefs.setString('password', _password!);
+
+      Fluttertoast.showToast(msg: 'Registration Successful!');
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Register')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (value) => _email = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Diperlukan Email';
+                  }
+                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Masukkan email yang valid';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                onSaved: (value) => _password = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Diperlukan Password';
+                  }
+                  if (value.length < 8) {
+                    return 'Password harus 8 karakter';
+                  }
+                  if (!RegExp(r'[0-9]').hasMatch(value)) {
+                    return 'Harus ada angka setidaknya 1';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _register,
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
